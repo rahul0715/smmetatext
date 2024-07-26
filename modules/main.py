5be7e03520a6dd65ff4018c38fc9008d6af2993f
rahul0715 <ajaychouhan26367@gmail.com>
2024-07-26 20:03:31 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -164,11 +164,7 @@ async def account_login(bot: Client, m: Message):
 
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
-            elif 'd26g5bnklkwsh4.cloudfront.net' in url or 'd1d34p8vz63oiq.cloudfront.net' in url:
-             id =  url.split("/")[-2]
-             url =  "http://d.alphastudyofficial.live/v2/alpha/pw/download?v=" + id + "/hls/" + raw_text2 + "/main.m3u8"
-
-
+             url =  "http://d.alphastudyofficial.live/v2/alpha/pw/download?v=https://d1d34p8vz63oiq.cloudfront.net/" + id + "/hls/" + raw_text2 + "/main.m3u8"
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
             name = f'{str(count).zfill(3)}) {name1[:60]}'
