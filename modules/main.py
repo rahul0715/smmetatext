3c676d37649312ccf8c1842787a1506ae4f7b9e3
rahul0715 <ajaychouhan26367@gmail.com>
2024-07-26 19:20:30 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -163,10 +163,10 @@ async def account_login(bot: Client, m: Message):
                 url   = response.json()['url']
 
             elif '/master.mpd' in url:
-                id =  url.split("/")[-2]
+             id =  url.split("/")[-2]
             elif 'd26g5bnklkwsh4.cloudfront.net' in url or 'd1d34p8vz63oiq.cloudfront.net' in url:
-                id =  url.split("/")[-2]
-                url =  "https://d.alphastudyofficial.live/v2/alpha/pw/download?v="
+             id =  url.split("/")[-2]
+             url =  "https://master-extract-api-8180936ccf13.herokuapp.com/?url={url}"
 
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
