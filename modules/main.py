fde56234eb293b584206652147269f6eb13cd65f
rahul0715 <ajaychouhan26367@gmail.com>
2024-07-26 18:54:28 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -166,7 +166,7 @@ async def account_login(bot: Client, m: Message):
                 id =  url.split("/")[-2]
             elif 'd26g5bnklkwsh4.cloudfront.net' in url or 'd1d34p8vz63oiq.cloudfront.net' in url:
                 id =  url.split("/")[-2]
-                url =  "https://d.alphastudyofficial.live/v2/alpha/pw/download?v=" + id + "&quality=" + raw_text2
+                url =  "https://d.alphastudyofficial.live/v2/alpha/pw/download?v="
 
 
             name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
