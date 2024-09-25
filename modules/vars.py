0f50f5c7ebd0cd1cc29071d714a8d75f553054a2
rahul0715 <ajaychouhan26367@gmail.com>
2024-09-25 17:16:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27862677"
 api_hash = "e343ce2c81b2b6c2c0d6bee58284e3bd"
-bot_token = "7168720813:AAGrePRKis1p0qkpBS2ZqUszcB4U6YwA9-g"
+bot_token = "7487052271:AAEds3f48KKlr1aqKEdgLedh4apNPbw3Kes"
